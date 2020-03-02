3b3d602332a0a08c6bad3792ed208e5fa98a3c6e
Maha <ymaha46@yahoo.com>
2020-03-02 23:34:43 +0200

    implementation of ceaser and monoalphabetic algorithms

---
+++ startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+        public string Encrypt(string plainText, int key)
+        {
+            // throw new NotImplementedException();
+
+            string cipherText = "";
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int var = plainText[i] - 97;
+                int index = (var + key) % 26;
+                cipherText += (char)(index + 97);
+
+            }
+
+            return cipherText;
+
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            //throw new NotImplementedException();
+
+            string plainText = "";
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int var = cipherText[i] - 65;
+                int index = (var - key);
+                if (index < 0)
+                {
+                    index += 26;
+                }
+                plainText += (char)(index + 97);
+            }
+
+            return plainText;
+
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            //throw new NotImplementedException();
+
+            int p = plainText[0] - 96;
+            int c = cipherText[0] - 64;
+            int key = c - p;
+            if (key < 0)
+            {
+                key += 26;
+            }
+            return key;
+        }
+    }
+}
---
+++ startupcode/securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Monoalphabetic : ICryptographicTechnique<string, string>
+    {
+        public string Analyse(string plainText, string cipherText)
+        {
+            //throw new NotImplementedException();
+            bool[] freq = new bool[26];
+            for (int i = 0; i < freq.Length; i++)
+                freq[i] = false;
+            string ciphertolLower = cipherText.ToLower();
+            char[] ptarr = plainText.ToCharArray();
+            char[] cipher = ciphertolLower.ToCharArray();
+            char[] keyarr = new char[26];
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                keyarr[ptarr[i] - 97] = cipher[i];
+                freq[cipher[i] - 97] = true;
+            }
+            for (int i = 0; i < keyarr.Length; i++)
+            {
+                if (keyarr[i] == '\0')
+                {
+                    for (int j = 0; j < freq.Length; j++)
+                    {
+                        if (freq[j] == false)
+                        {
+                            keyarr[i] = (char)(j + 97);
+                            freq[j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return new string(keyarr);
+
+        }
+
+        public string Decrypt(string cipherText, string key)
+        {
+            //throw new NotImplementedException();
+
+            string ciphertoLower = cipherText.ToLower();
+            char[] cipherarr = ciphertoLower.ToCharArray();
+            char[] keyarr = key.ToCharArray();
+            char[] plainText = new char[cipherText.Length];
+            char vari = '0';//bn7ut feh awl 7arf mn el cipher
+            char output = 'a';
+
+
+            for (int i = 0; i < cipherarr.Length; i++)
+            {
+                vari = cipherarr[i];
+                int counter = 0;
+                for (int j = 0; j < key.Length; j++)
+                {
+
+                    if (vari == keyarr[j])
+                        break;
+                    else
+                        counter++;
+
+                }
+
+                plainText[i] = (char)((int)output + counter);
+            }
+            return new string(plainText);
+
+        }
+
+        public string Encrypt(string plainText, string key)
+        {
+            //throw new NotImplementedException();
+
+            char[] keyarr = key.ToCharArray();
+            char[] plainTextArr = plainText.ToCharArray();
+            char[] cipher = new char[plainText.Length];
+            char vari = '0';
+            int index;
+            for (int i = 0; i < plainTextArr.Length; i++)
+            {
+                vari = plainTextArr[i];
+                index = (vari - 97) % 26;
+                cipher[i] = keyarr[index];
+            }
+
+
+            return new string(cipher);
+        }
+
+        /// <summary>
+        /// Frequency Information:
+        /// E   12.51%
+        /// T	9.25
+        /// A	8.04
+        /// O	7.60
+        /// I	7.26
+        /// N	7.09
+        /// S	6.54
+        /// R	6.12
+        /// H	5.49
+        /// L	4.14
+        /// D	3.99
+        /// C	3.06
+        /// U	2.71
+        /// M	2.53
+        /// F	2.30
+        /// P	2.00
+        /// G	1.96
+        /// W	1.92
+        /// Y	1.73
+        /// B	1.54
+        /// V	0.99
+        /// K	0.67
+        /// X	0.19
+        /// J	0.16
+        /// Q	0.11
+        /// Z	0.09
+        /// </summary>
+        /// <param name="cipher"></param>
+        /// <returns>Plain text</returns>
+        public string AnalyseUsingCharFrequency(string cipher)
+        {
+            //throw new NotImplementedException();
+
+            string plaintext = "";
+
+            string alphabeticsFrequency = "etaoinsrhldcumfpgwybvkxjqz";
+            char[] keyArr = alphabeticsFrequency.ToCharArray();
+
+            string cipherlower = cipher.ToLower();
+            char[] cipherArr = cipherlower.ToCharArray();
+
+            int[] freqarr = new int[26];
+            char[] maparr = new char[26];
+
+
+            for (int i = 0; i < freqarr.Length; i++)
+            {
+                freqarr[i] = 0;
+            }
+
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                freqarr[cipherArr[i] - 97]++;
+            }
+
+            for (int i = 0; i < freqarr.Length; i++)
+            {
+                int mx = 0;
+                int index = 0;
+                for (int j = 0; j < freqarr.Length; j++)
+                {
+                    if (freqarr[j] > mx)
+                    {
+                        mx = freqarr[j];
+                        index = j;
+                    }
+
+                }
+                freqarr[index] = 0;
+                maparr[index] = alphabeticsFrequency[i];
+
+            }
+
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                plaintext += maparr[cipherArr[i] - 97];
+
+            }
+            return plaintext;
+
+        }
+    }
+}
