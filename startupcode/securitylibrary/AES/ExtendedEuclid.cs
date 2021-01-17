5a3bab544238856342e8276137f99542a07cbd17
Maha Sayed <41696038+MahaSayed@users.noreply.github.com>
2021-01-17 18:35:30 +0200

    Update ExtendedEuclid.cs

--- startupcode/securitylibrary/AES/ExtendedEuclid.cs
+++ startupcode/securitylibrary/AES/ExtendedEuclid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SecurityLibrary.AES
 {
-    public class ExtendedEuclid 
+    public class ExtendedEuclid
     {
         /// <summary>
         /// 
@@ -16,7 +16,47 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int A1 = 1;
+            int A2 = 0;
+            int A3 = baseN;
+
+            int B1 = 0;
+            int B2 = 1;
+            int B3 = number;
+
+            double T1, T2, T3;
+
+            double Q;
+
+            while (true)
+            {
+                if (B3 == 0)
+                {
+                    return -1;
+                }
+                else if (B3 == 1)
+                {
+                    if (B2 < 0)
+                    {
+                        B2 += 26;
+                    }
+                    return B2;
+                }
+                Q = A3 / B3;
+
+                T1 = A1 - Q * B1;
+                T2 = A2 - Q * B2;
+                T3 = A3 - Q * B3;
+
+                A1 = B1;
+                A2 = B2;
+                A3 = B3;
+
+                B1 = (int)T1;
+                B2 = (int)T2;
+                B3 = (int)T3;
+            }
         }
     }
 }
