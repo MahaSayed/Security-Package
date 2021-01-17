4898837a4b8205ab6f1f3ea142938ac88be6d39f
Maha Sayed <41696038+MahaSayed@users.noreply.github.com>
2021-01-17 18:37:03 +0200

    Update DiffieHellman.cs

--- startupcode/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ startupcode/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,31 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int[] keys = new int[2];
+            int ya = 0, yb = 0;
+
+            //generating public key (ya)
+            ya = alpha % q;
+            for (int i = 1; i < xa; i++)
+                ya = (ya * alpha) % q;
+
+            //generating public key (yb)
+            yb = alpha % q;
+            for (int i = 1; i < xb; i++)
+                yb = (yb * alpha) % q;
+
+            //generating secret key by user A
+            keys[0] = yb % q;
+            for (int i = 1; i < xa; i++)
+                keys[0] = (keys[0] * yb) % q;
+
+            //generating secret key by user B
+            keys[1] = ya % q;
+            for (int i = 1; i < xb; i++)
+                keys[1] = (keys[1] * ya) % q;
+
+            return keys.ToList();
         }
     }
 }
