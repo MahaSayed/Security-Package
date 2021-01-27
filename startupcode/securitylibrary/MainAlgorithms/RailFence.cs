d695420be73d7538a4e857d6ee491fb861567845
Maha Sayed <41696038+MahaSayed@users.noreply.github.com>
2021-01-27 11:48:58 +0200

    Update RailFence.cs

--- startupcode/securitylibrary/MainAlgorithms/RailFence.cs
+++ startupcode/securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,136 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            int key = -1;
+
+            plainText = plainText.ToUpper();
+            cipherText = cipherText.ToUpper();
+            char end = cipherText[1];
+
+            int index = 1;
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                key++;
+                if (plainText[i] == end && plainText[i + 1] != cipherText[index])
+                    break;
+            }
+
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToUpper();
+            string pt = "";
+
+            if (cipherText.Contains(" "))
+            {
+                cipherText = cipherText.Replace(" ", "");
+            }
+
+
+            int lentext = cipherText.Length;
+
+
+            while (lentext % key != 0)
+            {
+                cipherText += " ";
+                lentext++;
+            }
+            int cols = lentext / key;
+
+
+            char[,] array = new char[key, cols];
+
+            int count = 0;
+
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = cipherText[count];
+                    count++;
+
+                }
+            }
+
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(array[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < key; j++)
+                {
+                    if (array[j, i].Equals(' '))
+                        break;
+                    pt += array[j, i];
+                }
+            }
+
+
+            return pt;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+
+            plainText = plainText.ToUpper();
+            string cp = "";
+
+            if (plainText.Contains(" "))
+            {
+                plainText = plainText.Replace(" ", "");
+            }
+
+
+            int lentext = plainText.Length;
+
+
+            while (lentext % key != 0)
+            {
+                plainText += " ";
+                lentext++;
+            }
+            int cols = lentext / key;
+
+
+            char[,] array = new char[key, cols];
+
+            int count = 0;
+
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < key; j++)
+                {
+                    array[j, i] = plainText[count];
+
+                    count++;
+                }
+            }
+
+
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (array[i, j].Equals(' '))
+                        break;
+                    cp += array[i, j];
+                    Console.Write(array[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+
+            return cp;
         }
     }
 }
